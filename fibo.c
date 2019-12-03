27dc0101d2614934e66b73ce1971cca8b5db737b
JC <jingdec2@illinois.edu>
2019-12-03 15:52:15 -0600

    minor changes

--- fibo.c
+++ fibo.c
@@ -2,7 +2,7 @@
 
 int main() {
     long long i, prev, curr, temp;
-    for (i = 0, prev = 0, curr = 1; i < 1000000; i++) {
+    for (i = 0, prev = 0, curr = 1; i < 100000000; i++) {
        temp = prev + curr;
        prev = curr;
        curr = temp;
