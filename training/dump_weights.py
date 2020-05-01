004aa417599edaecf8afa239bad91289853ec861
jc <jingdec2@illinois.edu>
2020-04-30 19:23:28 -0500

    Complete matmul and forward mlp in C. -cr

---
+++ training/dump_weights.py
@@ -0,0 +1,30 @@
+import sys
+import pickle
+import numpy
+
+tag = sys.argv[1]
+weight_file = 'pickle_' + tag + '.weights'
+
+fm = 0
+
+with open(weight_file, 'rb') as f:
+    weights = pickle.load(f)
+
+if fm == 0:
+    for weight_entry in weights:
+        for line in weight_entry:
+            try:
+                print(', '.join([f'{f: 8f}' for f in line]))
+            except:
+                print(line)
+elif fm == 1:
+    for weight_entry in weights:
+        ret = []
+        for line in weight_entry:
+            try:
+                for f in line:
+                    ret.append(f)
+            except:
+                ret.append(line)
+
+        print(', '.join([f'{f: 8f}' for f in ret]))
