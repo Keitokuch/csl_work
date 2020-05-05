ee7f9488fc05249fa715447924cc6007b9c2be53
jc <chenjd98@163.com>
2020-05-05 13:30:44 -0500

    impl hist imba. -cu

--- eval/hist_imbalance.py
+++ eval/hist_imbalance.py
@@ -9,7 +9,9 @@ import argparse
 def read_series(filename):
     with open(filename, 'r') as f:
         series = pd.Series(json.load(f))
-    return series
+    z_scores = zscore(series)
+    filtered = np.abs(z_scores) < 5
+    return series[filtered]
 
 
 parser = argparse.ArgumentParser()
@@ -18,7 +20,7 @@ parser.add_argument('-s', '--size', action='store')
 
 args = parser.parse_args()
 
-SAMPLE_SIZE = args.size or 2500
+#  SAMPLE_SIZE = args.size or 2500
 
 #  func = sys.argv[1]
 #  model = sys.argv[2]
@@ -36,9 +38,12 @@ def plot_hist(data, bins, color, label, alpha=0.65):
 if args.model:
     filename = f'imbalance_{args.model}.json'
 
-    imba = read_series(filename).sample(SAMPLE_SIZE)
+    #  imba = read_series(filename).sample(SAMPLE_SIZE)
+    imba = read_series(filename)
 
     n, bins, patches = plt.hist(imba, bins='auto', alpha=0.7, rwidth=1)
+    print(n)
+    plt.xticks(np.arange(min(imba), max(imba)+1, 1.0))
     plt.grid(axis='y', alpha=0.6)
     plt.xlabel('Max Imbalance (jobs)')
     plt.ylabel('Frequency')
@@ -47,18 +52,18 @@ if args.model:
 else:
     data = []
     for model in ['mlp', 'linux']:
-        filename = f'latency_{args.func}_{model}{args.thread}.json'
-        latencies = read_series(filename).sample(SAMPLE_SIZE)
-        print(model, len(latencies))
-        data.append(latencies)
-    bins = SAMPLE_SIZE // 5000
-    _, _, _ = plot_hist(data[1], bins=bins, color='tab:blue', label='Linux')
+        filename = f'imbalance_{model}.json'
+        imba = read_series(filename).sample(SAMPLE_SIZE)
+        print(model, len(imba))
+        data.append(imba)
+    #  bins = SAMPLE_SIZE // 5000
+    n, bins, _ = plot_hist(data[1], bins='auto', color='tab:blue', label='Linux')
     plot_hist(data[0], bins=bins, color='tab:orange', label='ML')
     plt.xlim(right=max(data[0].max(), data[1].max()))
     plt.grid(axis='y', alpha=0.4)
     plt.legend(fontsize='small')
-    plt.xlabel('Latency (ns)')
+    plt.xlabel('Max Imbalance (ns)')
     plt.ylabel('Frequency')
-    plt.title('Latency of function {}'.format(FUNC_NAME))
+    plt.title('Histogram of Max Imbalance of models')
 
     plt.show()
