fd0f2b84bfdee2c291224027dcd2fa53e5754588
jc <jingdec2@illinois.edu>
2020-05-05 15:21:28 +0000

    add latency ana. -r

---
+++ eval/lat_ana.py
@@ -0,0 +1,15 @@
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import numpy as np
+
+
+filename = sys.argv[1]
+
+with open(filename, 'r') as f:
+    latencies = pd.Series(json.load(f))
+
+print(len(latencies))
+print(latencies.mean())
+print(latencies.std())
