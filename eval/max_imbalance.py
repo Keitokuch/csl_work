3dffb98f87cfebb2a7ec47f1cdad34e862930eb2
jc <chenjd98@163.com>
2020-05-04 23:41:25 -0500

    complete max imba. -u

--- eval/max_imbalance.py
+++ eval/max_imbalance.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-from bcc import BPF
+from bcc import BPF, PerfType, PerfSWConfig
+from time import sleep
 import sys
 import logging
 import argparse
-
-from datasource import FuncLatencyDatasource
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--tag', help='tag for output')
@@ -13,43 +12,40 @@ parser.add_argument('-o', '--output', help='output file name (overwrites -t)')
 parser.add_argument('-a', '--append', action='store_true', help='append to output')
 args = parser.parse_args()
 
+frequency = 500
+
 write_file = args.output or args.tag and f'imbalance_{args.tag}.json' or 'imbalance.json'
 
-cm_events = []
-latency_datasource = FuncLatencyDatasource(append=args.append,
-                                           write_file=write_file)
+#  cm_events = []
+#  latency_datasource = FuncLatencyDatasource(append=args.append,
+#                                             write_file=write_file)
 
 bpf_text = "max_imbalance.c"
 
 # initialize BPF & probes
 b = BPF(src_file=bpf_text)
-b.attach_kprobe(event='can_migrate_task', fn_name='trace_func_enter')
-b.attach_kretprobe(event='can_migrate_task', fn_name='trace_func_return')
+b.attach_perf_event(ev_type=PerfType.SOFTWARE,
+    ev_config=PerfSWConfig.CPU_CLOCK, fn_name="do_perf_event",
+    sample_period=0, sample_freq=frequency)
 
 
-def funclatency_handler(cpu, data, size):
-    event = b['bpf_output'].event(data)
-    cm_events.append(event)
-
-
-# set up perf buffers
-b['bpf_output'].open_perf_buffer(funclatency_handler, page_cnt=256)
 
 logger = logging.getLogger('dump')
 fhdlr = logging.FileHandler('dump.log')
 shdlr = logging.StreamHandler()
 logger.addHandler(fhdlr)
 logger.addHandler(shdlr)
+
+hist = b.get_table("hist")
 while True:
     try:
-        b.perf_buffer_poll()
-        for event in cm_events:
-            latency_datasource.update(event)
-        cm_events = []
+        sleep(0.5)
     except KeyboardInterrupt:
-        print(latency_datasource.dump())
         exit()
-    except Exception as e:
-        print(e)
-        exit()
-        logger.warn(e)
+
+    rqlen = [k.value for k in hist]
+    print(rqlen)
+    maxlen, minlen = max(rqlen), min(rqlen)
+    imbalance = maxlen - minlen
+    print(imbalance)
+    hist.clear()
