a6bf7c80638f5addaa1eba0786468fa7565edca6
jc <chenjd98@163.com>
2020-05-05 00:07:07 -0500

    complete hist laten. -rb

--- eval/hist_latency.py
+++ eval/hist_latency.py
@@ -1,16 +1,29 @@
 import json
 import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import numpy as np
+from scipy.stats import zscore
 
-filename='output'
+SAMPLE_SIZE = 100000
+
+func = sys.argv[1]
+model = sys.argv[2]
+
+filename = f'latency_{func}_{model}.json'
 
 with open(filename, 'r') as f:
-    latencies = json.load(f)
+    latencies = pd.Series(json.load(f))
+
+z_scores = zscore(latencies)
+filtered_entries = np.abs(z_scores) < 2
+latencies = latencies[filtered_entries].sample(SAMPLE_SIZE)
 
 n, bins, patches = plt.hist(x=latencies, bins='auto', color='#0504aa',
-                            alpha=0.7, rwidth=0.85)
+                            alpha=0.7, rwidth=1)
 plt.grid(axis='y', alpha=0.75)
 plt.xlabel('Value')
 plt.ylabel('Frequency')
 plt.title('My Very Own Histogram')
-plt.text(23, 45, r'$\mu=15, b=3$')
+plt.text(1000, 1000, f'mean={latencies.mean():.0f}, std={latencies.std():.0f}')
 plt.show()
