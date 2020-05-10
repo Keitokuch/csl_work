8158d300ba5f29cfa954d0e07fcd01d8635da561
jc <chenjd98@163.com>
2020-05-09 23:37:25 -0500

    hist lat

--- eval/hist_latency.py
+++ eval/hist_latency.py
@@ -23,15 +23,15 @@ args = parser.parse_args()
 SAMPLE_SIZE = 1200000 if args.thread == '80' else 100000
 FUNC_NAME = 'can_migrate_task' if args.func == 'cm' else 'load_balance'
 
+
 #  func = sys.argv[1]
 #  model = sys.argv[2]
 #  th = sys.argv[3]
-def plot_hist(data, bins, color, label, alpha=0.65):
+def plot_hist(data, bins, label, color=None, alpha=0.75):
     avg = data.mean()
     std = data.std()
     label = f'{label}\nmean={avg:.0f},std={std:.0f}'
     n, bins, patches = plt.hist(data, bins=bins, density=True, color=color, alpha=alpha, label=label)
-    plt.xscale('log')
     plt.axvline(avg, color=color, linestyle='-', linewidth=1, alpha=alpha+0.2)
     plt.axvline(avg+std, color=color, linestyle='--', linewidth=1, alpha=alpha)
     plt.axvline(avg-std, color=color, linestyle='--', linewidth=1, alpha=alpha)
@@ -53,14 +53,19 @@ if args.model:
     plt.show()
 else:
     data = []
-    for model in ['mlp', 'linux']:
+    for model in ['linux', 'MLP', 'fxdpt']:
         filename = f'latency_{args.func}_{model}{args.thread}.json'
         latencies = read_series(filename) #.sample(SAMPLE_SIZE)
         print(model, len(latencies))
         data.append(latencies)
-    bins = 300
-    _, _, _ = plot_hist(data[1], bins=bins, color='tab:blue', label='Linux')
-    plot_hist(data[0], bins=bins, color='tab:orange', label='ML')
+    bins = 100
+    nr_bin = 50
+    right = max(map(max, data))
+    bins = range(0, right, right // nr_bin)
+    n, newbins, _ = plot_hist(data[0], bins=bins, color='tab:blue', label='Linux')
+    plot_hist(data[1], bins=newbins, color='tab:orange', label='ML')
+    plot_hist(data[2], bins=newbins, color='tab:pink', label='FixedPt')
+    #  plt.hist(data, bins=bins, label=['Linux', 'MLP', 'FixedPt'], density=True)
     plt.xlim(right=max(data[0].max(), data[1].max()))
     plt.grid(axis='y', alpha=0.4)
     plt.legend(fontsize='small')
