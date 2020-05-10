d9267389bcfcffc4aa37a5173dd3de6dd27f5198
jc <chenjd98@163.com>
2020-05-10 00:05:16 -0500

    up hist lat

--- eval/hist_latency.py
+++ eval/hist_latency.py
@@ -32,6 +32,7 @@ def plot_hist(data, bins, label, color=None, alpha=0.75):
     std = data.std()
     label = f'{label}\nmean={avg:.0f},std={std:.0f}'
     n, bins, patches = plt.hist(data, bins=bins, density=True, color=color, alpha=alpha, label=label)
+    #  color='tab:red'
     plt.axvline(avg, color=color, linestyle='-', linewidth=1, alpha=alpha+0.2)
     plt.axvline(avg+std, color=color, linestyle='--', linewidth=1, alpha=alpha)
     plt.axvline(avg-std, color=color, linestyle='--', linewidth=1, alpha=alpha)
@@ -58,19 +59,21 @@ else:
         latencies = read_series(filename) #.sample(SAMPLE_SIZE)
         print(model, len(latencies))
         data.append(latencies)
-    bins = 100
-    nr_bin = 50
+    nr_bin = 100
     right = max(map(max, data))
     bins = range(0, right, right // nr_bin)
-    n, newbins, _ = plot_hist(data[0], bins=bins, color='tab:blue', label='Linux')
-    plot_hist(data[1], bins=newbins, color='tab:orange', label='ML')
-    plot_hist(data[2], bins=newbins, color='tab:pink', label='FixedPt')
+    #  plot_hist(data[0], bins=bins, color='tab:blue', label='Linux')
+    #  plot_hist(data[1], bins=bins, color='tab:orange', label='MLP Floating Point')
+    plot_hist(data[2], bins=bins, color='tab:pink', label='MLP Fixed Point')
     #  plt.hist(data, bins=bins, label=['Linux', 'MLP', 'FixedPt'], density=True)
     plt.xlim(right=max(data[0].max(), data[1].max()))
+    plt.ylim(top=0.0015)
     plt.grid(axis='y', alpha=0.4)
     plt.legend(fontsize='small')
     plt.xlabel('Latency (ns)')
-    plt.ylabel('Frequency')
-    plt.title('Latency of function {}'.format(FUNC_NAME))
+    plt.ylabel('Density')
+    #  plt.title('Latency of function {} with {}'.format(FUNC_NAME, 'Linux'))
+    #  plt.title('Latency of function {} with {}'.format(FUNC_NAME, 'MLP in Floating Point'))
+    plt.title('Latency of function {} with {}'.format(FUNC_NAME, 'MLP in Fixed Point'))
 
     plt.show()
